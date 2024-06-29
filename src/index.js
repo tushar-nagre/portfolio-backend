da5e2f39d4fc2faac3710a5616fca386fed742c9
Tushar <tushar.nagre@torum.com>
2024-06-29 11:17:03 +0530

    Utils setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        try {
+            app.listen(process.env.PORT || 4000, () => {
+                console.log(`Server is running on port : ${process.env.PORT}`);
+            })
+        } catch (error) {
+            console.log(`App is not listning on port!!! ${error.stack}`)
+        }
+    })
+    .catch((error) => {
+        console.log(`DB connection failed !!! ${error.stack}`)
+    })
